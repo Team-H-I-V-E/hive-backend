7f464213c035d117a675baf6f237981102f40dcd
dannielhso <dannielha@naver.com>
2025-03-01 17:08:30 +0900

    feat : getAllPanoramaViewers 기능 작성.

--- src/panorama/controller/panoramaViewer.controller.ts
+++ src/panorama/controller/panoramaViewer.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from '@nestjs/common';//auto import
+import { Controller, Get } from '@nestjs/common';//auto import
+import { PanoramaViewerService } from '../service/panoramaViewer.service';
+import { PanoramaViewer } from '../entities/panoramaViewer.entity';
 
-@Controller('boards')
+@Controller('api/panoramaViewers')
 export class PanoramaViewerController {
+
+    constructor(private panoramaViewerService: PanoramaViewerService) {}
+
+    @Get('/')
+    getAllPanoramaViewers(): PanoramaViewer[] {
+        return this.panoramaViewerService.getAllPanoramaViewers();
+    }
 
 }
--- src/panorama/service/panoramaViewer.service.ts
+++ src/panorama/service/panoramaViewer.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PanoramaViewer } from '../entities/panoramaViewer.entity';
 
 @Injectable()
 export class PanoramaViewerService {
     
+    private panoramaViewers: PanoramaViewer[] = [];
+
+    getAllPanoramaViewers(): PanoramaViewer[] {
+        return this.panoramaViewers;
+    }
+
 }
