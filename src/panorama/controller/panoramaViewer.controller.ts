6eebd6e1ccfc3f3df72d935635112ea1e060237d
dannielhso <dannielha@naver.com>
2025-03-07 16:13:07 +0900

    feat : Panorama 즐겨찾기 기능 추가 (📜 Panorama #11)

--- src/panorama/controller/panoramaViewer.controller.ts
+++ src/panorama/controller/panoramaViewer.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';//auto import
+import { Body, Controller, Get, Post } from '@nestjs/common';//auto import
 import { PanoramaViewerService } from '../service/panoramaViewer.service';
 import { PanoramaViewer } from '../entities/panoramaViewer.entity';
+import { PanoramaFavoriteRequestDto } from '../dto/panoramaFavorite/panoramaFavorite-request.dto';
 
 @Controller('api/panoramaViewers')
 export class PanoramaViewerController {
@@ -9,6 +10,11 @@ export class PanoramaViewerController {
     @Get('/')
     getAllPanoramaViewers(): PanoramaViewer[] {
         return this.panoramaViewerService.getAllPanoramaViewers();
+    }
+
+    @Post('/') // PostMapping 핸들러 데코레이터
+    createBoard(@Body() panoramaFavoriteRequestDto: PanoramaFavoriteRequestDto)  {
+        return this.panoramaViewerService.addPanoramaFavorite(panoramaFavoriteRequestDto);
     }
 
 }
--- src/panorama/service/panoramaViewer.service.ts
+++ src/panorama/service/panoramaViewer.service.ts
@@ -1,12 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PanoramaViewer } from '../entities/panoramaViewer.entity';
+import { PanoramaFavoriteRequestDto } from '../dto/panoramaFavorite/panoramaFavorite-request.dto';
+import { panoramaFavorite } from '../entities/panoramaViewerFavorite.entity';
 
 @Injectable()
 export class PanoramaViewerService {
     private panoramaViewers: PanoramaViewer[] = [];
+    private panoramaFavorite: panoramaFavorite[] = [];
 
     getAllPanoramaViewers(): PanoramaViewer[] {
         return this.panoramaViewers;
     }
+
+    addPanoramaFavorite(panoramaFavoriteRequestDto: PanoramaFavoriteRequestDto) {
+        const {panoramaFavoriteID, userID, panoramaViewerID, } = panoramaFavoriteRequestDto;
+
+        const favorite: panoramaFavorite = {
+            panoramaFavoriteID,
+            userID,
+            panoramaViewerID,
+            panoramaFavoriteCreatedAt: new Date,
+        }
+        this.panoramaFavorite.push(favorite);
+        return favorite;
+    }
+    
 
 }
