42a33432fca85477fda68dc933ff783b86bd9937
dannielhso <dannielha@naver.com>
2025-03-08 14:09:46 +0900

    chore : DBconfig, TypeORMconfig 작성 및 의존 관계 설정 (📜 Panorama #11)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PanoramaViewerModule } from './panorama/panorama.module';
+import { databaseConfig } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 
 @Module({
-  imports: [PanoramaViewerModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    PanoramaViewerModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide : 'DATABASE_CONFIG',
+      useValue: databaseConfig
+    },
+  ],
+  exports: ['DATABASE_CONFIG']
 })
 export class AppModule {}
--- src/panorama/panorama.module.ts
+++ src/panorama/panorama.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PanoramaViewerController } from './controller/panoramaViewer.controller';
 import { PanoramaViewerService } from './service/panoramaViewer.service';
+import { PanoramaViewerRepository } from './repository/panoramaViewer-repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PanoramaViewer } from './entities/panoramaViewer.entity';
+import { PanoramaFavorite } from './entities/panoramaViewerFavorite.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PanoramaViewer, PanoramaFavorite])
+  ],
   controllers: [PanoramaViewerController],
-  providers: [PanoramaViewerService],
+  providers: [
+    PanoramaViewerService, 
+    PanoramaViewerRepository,
+  ],
 })
 export class PanoramaViewerModule {}
