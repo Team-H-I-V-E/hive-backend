b21c866de90c019e1348deb789a0a00e25356828
dannielhso <dannielha@naver.com>
2025-03-08 14:05:16 +0900

    refactor : Service 계층 분리 (📜 Panorama #11)
    PanoramaViewerService 와 PanoramaViewerFavoriteService 로 분리

--- src/panorama/service/panoramaViewer.service.ts
+++ src/panorama/service/panoramaViewer.service.ts
@@ -1,41 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { PanoramaViewer } from '../entities/panoramaViewer.entity';
-import { PanoramaFavoriteRequestDto } from '../dto/panoramaFavorite/panoramaFavorite-request.dto';
-import { panoramaFavorite } from '../entities/panoramaViewerFavorite.entity';
+import { PanoramaViewerRepository } from '../repository/panoramaViewer-repository';
 
 @Injectable()
 export class PanoramaViewerService {
-    private panoramaViewers: PanoramaViewer[] = [];
-    private panoramaFavorites: panoramaFavorite[] = [];
+    constructor(private panoramaViewerRepository: PanoramaViewerRepository) {}
 
-    getAllPanoramaViewers(): PanoramaViewer[] {
-        return this.panoramaViewers;
-    }
-
-    addPanoramaFavorite(panoramaFavoriteRequestDto: PanoramaFavoriteRequestDto) {
-        const {userID, panoramaViewerID, } = panoramaFavoriteRequestDto;
-
-        const favorite: panoramaFavorite = {
-            panoramaFavoriteID: this.panoramaFavorites.length + 1,
-            userID,
-            panoramaViewerID,
-            panoramaFavoriteCreatedAt: new Date,
-        }
-        this.panoramaFavorites.push(favorite);
-        return favorite;
-    }
-
-    foundPanoramaFavoriteByID(panoramaFavoriteID: number): panoramaFavorite {
-        const favorite = this.panoramaFavorites.find((favorite) => favorite.panoramaFavoriteID === panoramaFavoriteID)
-        if (!favorite) {
-            throw new Error(`PanoramaFavorite with ID ${panoramaFavoriteID} not found.`);
-        }
-        return favorite;
-    }
-    
-    deletePanoramaFavorite(panoramaFavoriteID: number): void {
-        const foundPanoramaFavorite = this.foundPanoramaFavoriteByID(panoramaFavoriteID);
-        this.panoramaFavorites = this.panoramaFavorites.filter((favorite) => favorite.panoramaFavoriteID !== foundPanoramaFavorite.panoramaFavoriteID);
+    async getAllPanoramaViewers(): Promise<PanoramaViewer[]> {
+        const foundPanoramaViewer = await this.panoramaViewerRepository.findAll();
+        return foundPanoramaViewer;
     }
 
 }
---
+++ src/panorama/service/panoramaViewerFavorite.service.ts
@@ -0,0 +1,41 @@
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { PanoramaFavoriteRequestDto } from "../dto/panoramaFavorite/panoramaFavorite-request.dto";
+import { PanoramaFavorite } from "../entities/panoramaViewerFavorite.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class PanoramaViewerFavoriteService {
+    constructor(
+        @InjectRepository(PanoramaFavorite)
+        private panoramaFavoriteRepository: Repository<PanoramaFavorite>
+    ) {}
+
+    async addPanoramaFavorite(panoramaFavoriteRequestDto: PanoramaFavoriteRequestDto): Promise<void> {
+        const {userID, panoramaViewerID, } = panoramaFavoriteRequestDto;
+        if (!userID || !panoramaViewerID) {
+            throw new BadRequestException('userID and panoramaViewerID contents must be provided');
+        }
+        const favorite = this.panoramaFavoriteRepository.create({
+            panoramaFavoriteID: 0,
+            userID,
+            panoramaViewerID,
+            panoramaFavoriteCreatedAt: new Date,
+        });
+        await this.panoramaFavoriteRepository.save(favorite);
+    }
+
+    async foundPanoramaFavoriteByID(panoramaFavoriteID: number): Promise<PanoramaFavorite> {
+        const foundFavorite = await this.panoramaFavoriteRepository.findOneBy({ panoramaFavoriteID: panoramaFavoriteID });
+        if (!foundFavorite) {
+            throw new Error(`PanoramaFavorite with ID ${panoramaFavoriteID} not found.`);
+        }
+        return foundFavorite;
+    }
+    
+    async deletePanoramaFavorite(panoramaFavoriteID: number): Promise<void> {
+        const foundPanoramaFavorite = await this.foundPanoramaFavoriteByID(panoramaFavoriteID);
+        await this.panoramaFavoriteRepository.remove(foundPanoramaFavorite)
+    }
+
+}
