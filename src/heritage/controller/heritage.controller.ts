651d9ff89cb2633ce7fcc5bea715deb4e16d1613
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-12 22:47:28 +0900

    feat: 유물 정보 조회 코드 추가 및 공공데이터 key를 이용해 데이터베이스에 저장 코드 구현  (📜 Heritage2 #14)

--- src/heritage/controller/heritage.controller.ts
+++ src/heritage/controller/heritage.controller.ts
@@ -9,34 +9,45 @@ import { UpdateHeritageDto } from '../dto/heritage/update-heritage.dto.';
 export class HeritageController {
     constructor(private readonly heritageService: HeritageService) { }
 
+    //유물 생성
     @Post()
     async createHeritage(@Body() createHeritageDto: CreateHeritageDto): Promise<Heritage> {
         return this.heritageService.createHeritage(createHeritageDto);
     }
 
+    //유물 전체 조회
     @Get()
-    async findAll(): Promise<Heritage[]> {
-        return this.heritageService.findAll();
+    async getHeritageList(): Promise<Heritage[]> {
+        return this.heritageService.getHeritageList();
     }
 
+    //유물 특정 조회  
     @Get(':id')
-    async findOne(@Param('id') id: number): Promise<Heritage> {
-        const heritage = await this.heritageService.findOne(id);
+    async getHeritageById(@Param('id') id: number): Promise<Heritage> {
+        const heritage = await this.heritageService.getHeritageById(id);
         if (!heritage) {
             throw new NotFoundException(`Heritage with ID ${id} not found`);
         }
         return heritage;
     }
-    
+
+    //유물 업데이트 
     @Patch(':id')
     async updateHeritage(@Param('id') id: number, @Body() updateHeritageDto: UpdateHeritageDto): Promise<Heritage> {
         return this.heritageService.updateHeritage(id, updateHeritageDto);
     }
 
+    //유물 삭제 
     @Delete(':id')
     async deleteHeritage(@Param('id') id: number): Promise<void> {
         return this.heritageService.deleteHeritage(id);
     }
 
-    
+    //유물 데이터 확인
+    @Post('fetch')
+    async fetchHeritageData() {
+        await this.heritageService.fetchHeritageData();
+        return { message: '세종시 유물 데이터 가져오기를 완료 했습니다' };
+    }
+
 }
