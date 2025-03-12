887291e603f4a8fdbe02c4ec25ee65931853aea6
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-12 22:24:03 +0900

    feat: 환경 변수 설정 및 TypeORM 구성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Heritage } from './heritage/entities/heritage.entity';
+import { HeritageModule } from './heritage/heritage.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+
+      isGlobal: true, //환경 변수를 전역으로 설정
+      envFilePath: '.env',
+
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'), 
+        database: configService.get<string>('DB_NAME'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        entities: [Heritage],
+        synchronize: true,
+        logging: true,
+        timezone: 'local',
+      }),
+    }),
+    
+    HeritageModule,
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
+
