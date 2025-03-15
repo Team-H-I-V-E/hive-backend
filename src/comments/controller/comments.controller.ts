f3427de0ce70d4598875adcb2487ed9e2516dedc
dannielhso <dannielha@naver.com>
2025-03-15 09:26:44 +0900

    feat : Comments 조회,작성 및 삭제 기능 추가

--- src/comments/controller/comments.controller.ts
+++ src/comments/controller/comments.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from "@nestjs/common";
-import { CommentService } from "../service/comments.service";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { CommentsService } from "../service/comments.service";
+import { Comments } from "../entities/comments.entity";
+import { CommentsResponseDto } from "../dto/comments-response.dto";
+import { CreateCommentDto } from "../dto/create-comment.dto";
 
 @Controller('api/comment')
-export class ArticlesController {
-    constructor(private commentService: CommentService){}
+export class CommentsController {
+    constructor(private commentsService: CommentsService){}
+
+    @Get('/')
+    async getAllComments(): Promise<CommentsResponseDto[]> {
+	    const comments: Comments[] = await this.commentsService.getAllBComments();
+        const commentsResponseDto = comments.map(comment => new CommentsResponseDto(comment));
+        return commentsResponseDto;
+    }
+
+    @Post('/')
+    async createBoard(@Body() createCommentDto: CreateCommentDto): Promise<CommentsResponseDto> {
+        const commentsResponseDto = new CommentsResponseDto(await this.commentsService.createBComment(createCommentDto))
+        return commentsResponseDto;
+    }
+
+    @Delete('/:id')
+    async deleteBoardById(@Param('id') id: number): Promise<void> {
+        await this.commentsService.deleteCommentById(id);
+    }
+
 }
