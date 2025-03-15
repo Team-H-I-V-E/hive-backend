0cad18be8f29206715d32c843e3691a17d9a46f2
dannielhso <dannielha@naver.com>
2025-03-15 15:58:31 +0900

    chore : AppModule에 의존성 주입 (#22)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ArticleModule } from './article/article.module';
 import { PanoramaViewerModule } from './panorama/panorama.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/typeorm.config';
+import { ArticleFavoriteModule } from './favorite/articleFavorite.module';
 
 
 @Module({
@@ -10,6 +11,7 @@ import { typeOrmConfig } from './configs/typeorm.config';
     TypeOrmModule.forRoot(typeOrmConfig),
     ArticleModule,
     PanoramaViewerModule,
+    ArticleFavoriteModule,
   ],
   controllers: [],
   providers: []
