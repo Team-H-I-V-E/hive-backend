2bb27414987953b14d50f76292583f2a30cefdfe
Kwahk <mjkwahk@gmail.com>
2025-03-16 11:15:45 +0900

    feat : stamp, collectedStamp 엔터티 생성 (#17)

---
+++ src/arExperience/entities/stamp.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Stamp {
+  @PrimaryGeneratedColumn()
+  stampID: number;
+
+  @Column()
+  panoramaViewerID: number;
+
+  @Column()
+  stampImage: string;
+
+  @Column('point')
+  stampCoordinate: string; // 위도, 경도
+}
