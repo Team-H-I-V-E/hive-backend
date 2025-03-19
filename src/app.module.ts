0d525c4732b594c31c1b3536b0d991c7ee1ace52
SMK <coz0703@naver.com>
2025-03-19 16:04:33 +0900

    feat: validationPipe 전역 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './configs/typeorm.config'; 
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -10,5 +11,11 @@ import { typeOrmConfig } from './configs/typeorm.config';
     UserModule,
     AuthModule,
   ],
+  providers: [
+    {
+        provide: APP_PIPE,
+        useClass: ValidationPipe,
+    },
+  ]
 })
 export class AppModule {}
