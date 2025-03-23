499bcefeefb73b25d967b4d9f3b2f9a50fe89e86
SMK <coz0703@naver.com>
2025-03-23 17:59:41 +0900

    feat: auth.controller.ts 로그인 api 기능 추가

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,24 +1,23 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from 'src/auth/service/auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user-request.dto';
-import { UserResponseDto } from 'src/user/dto/user-response.dto'; // 
-import { UserService } from 'src/user/user.service';
+import { LoginUserDto } from 'src/user/dto/login-user.dto';
+import { UserResponseDto } from 'src/user/dto/user-response.dto';
 
 @Controller('api/auth')
 export class AuthController {
     constructor(
-        private authService: AuthService,
-    ) {}
+        private authService: AuthService) {}
 
     // 회원 가입 기능
     @Post('/signup')
-    async createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-        console.log(createUserDto);
+    createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
+        return this.authService.createUser(createUserDto);
+    }
 
-        if (!createUserDto || !createUserDto.userEmail) {
-            throw new BadRequestException('잘못된 요청입니다. DTO 확인 필요.');
-        }
-        const user = await this.authService.createUser(createUserDto);
-        return new UserResponseDto(user);
+    // 로그인 기능
+    @Post('/signin')
+    logIn(@Body() loginUserDto: LoginUserDto) {
+        return this.authService.logIn(loginUserDto);
     }
 }
