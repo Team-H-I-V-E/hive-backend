84b7fd30652754d8a3c13ffb3fa94896314dbe9d
Kwahk <mjkwahk@gmail.com>
2025-03-23 20:38:35 +0900

    fix : 중복 스탬프 획득 방지 로직 수정 및 코드 개선 (#36)

--- src/arExperience/service/arExperience.service.ts
+++ src/arExperience/service/arExperience.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CollectedStamp } from '../entities/collectedStamp.entity';
 import { AcquireStampDto } from '../dto/arExperience-request.dto';
 import { Stamp } from '../entities/stamp.entity';
+
 @Injectable()
 export class ArExperienceService {
   constructor(
@@ -19,17 +20,7 @@ export class ArExperienceService {
     const collectedStamps = await this.collectedStampRepository.find({ where: { userID: userId } });
 
     // 획득한 스탬프가 없으면 빈 배열 반환
-    if (collectedStamps.length === 0) {
-      return [];
-    }
-
-    return collectedStamps.map(collectedStamp => ({
-      userID: collectedStamp.userID,
-      collectedStampID: collectedStamp.collectedStampID,
-      stampID: collectedStamp.stampID,
-      stampTime: collectedStamp.stampTime,
-      isAcquired: true,
-    }));
+    return collectedStamps.length > 0 ? collectedStamps : [];
   }
 
   async getUnacquiredStamps(userId: number) {
@@ -41,28 +32,15 @@ export class ArExperienceService {
     const collectedStampIds = new Set(collectedStamps.map(stamp => stamp.stampID));
 
     // 전체 스탬프 목록에 대해 획득 여부를 설정
-    const stampsWithStatus = allStamps.map(stamp => ({
-      stampID: stamp.stampID,
-      stampImage: stamp.stampImage,
-      stampLatitude: stamp.stampLatitude,
-      stampLongitude: stamp.stampLongitude,
-      isAcquired: collectedStampIds.has(stamp.stampID),
-    }));
-
-    // 획득하지 않은 스탬프 필터링
-    const unacquiredStamps = stampsWithStatus.filter(stamp => !stamp.isAcquired);
-    return unacquiredStamps;
+    return allStamps.filter(stamp => !collectedStampIds.has(stamp.stampID));
   }
   
   async acquireStamp(acquireStampDto: AcquireStampDto) {
     const { userID, stampID, userLatitude, userLongitude } = acquireStampDto;
 
-    // 사용자가 획득한 스탬프 목록 조회
-    const collectedStamps = await this.getStamps(userID);
-    const collectedStampIds = new Set(collectedStamps.map(stamp => stamp.stampID));
-
     // 이미 획득한 스탬프인지 확인
-    if (collectedStampIds.has(stampID)) {
+    const existingStamp = await this.collectedStampRepository.findOne({ where: { userID, stampID } });
+    if (existingStamp) {
         throw new BadRequestException('Stamp has already been acquired');
     }
 
