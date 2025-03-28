dcb1acd36d51d26b21b7b77ca562a9c4f5131cd4
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-28 19:01:32 +0900

    refactor: 파일 업로드 서비스 관련 코드 추가 및 수정 (📜 Heritage5 #32)

--- src/heritage/controller/heritage.3dModel.controller.ts
+++ src/heritage/controller/heritage.3dModel.controller.ts
@@ -1,30 +1,28 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Param, Body, BadRequestException } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Body, BadRequestException } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { multerOptions } from 'src/configs/upload.config';
 import { Heritage3DModelService } from '../service/heritage3dModel.service';
-
 
 @Controller('api/heritage-3d-models')
 export class Heritage3DModelController {
-  constructor(private readonly heritage3DModelService: Heritage3DModelService) {}
+  constructor(private readonly heritage3dModelSerivce: Heritage3DModelService) {}
 
   // 3D 모델 업로드 API
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file', multerOptions))
+  @UseInterceptors(FileInterceptor('file')) // multerOptions 제거
   async upload3dModel(
     @Body('heritageId') heritageId: number,
-    @UploadedFile() file: Express.Multer.File) {
+    @UploadedFile() file: Express.Multer.File
+  ) {
     if (!file) {
       throw new BadRequestException('파일이 업로드되지 않았습니다.');
     }
 
-    // 업로드 후 응답 데이터 생성
-    const response = await this.heritage3DModelService.createHeritage3DModel(heritageId, file.filename);
-    
-    return {
-      message: '파일이 성공적으로 업로드 되었습니다.',
-      modelFileUrl: `/uploads/3d-models/${file.filename}`,
-    }
+    // FileService를 통해 파일 저장
+    const uploadResult = await this.heritage3dModelSerivce.uploadFile(file);
 
+    return {
+      message: '파일이 성공적으로 업로드되었습니다.',
+      modelFileUrl: uploadResult.filePath, // 저장된 파일의 경로 반환
+    };
   }
 }
