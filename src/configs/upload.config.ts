7547c2bccc9885f70493bddc3505e0707872496b
kdk1248 <alice3976@g.hongik.ac.kr>
2025-03-28 19:04:07 +0900

    refactor: config -> service 파일로 처리 (📜 Heritage5 #32)

--- src/configs/upload.config.ts
+++ src/configs/upload.config.ts
@@ -1,22 +1,22 @@
-import { diskStorage } from 'multer';
-import { extname, join } from 'path';
-import * as fs from 'fs';
+// import { diskStorage } from 'multer';
+// import { extname, join } from 'path';
+// import * as fs from 'fs';
 
-export const multerOptions = {
-  storage: diskStorage({
-    destination: (req, file, cb) => {
-      const modelId = req.params.id; // heritage3dModelId
-      const uploadPath = join(__dirname, `../../uploads/3d-models/${modelId}`);
+// export const multerOptions = {
+//   storage: diskStorage({
+//     destination: (req, file, cb) => {
+//       const heritageId = req.body.heritageId;
+//       const uploadPath = join(__dirname, `../../uploads/3d-models/${heritageId}`);
 
-      // 디렉터리가 존재하지 않으면 생성
-      if (!fs.existsSync(uploadPath)) {
-        fs.mkdirSync(uploadPath, { recursive: true });
-      }
+//       // 디렉터리가 존재하지 않으면 생성
+//       if (!fs.existsSync(uploadPath)) {
+//         fs.mkdirSync(uploadPath, { recursive: true });
+//       }
 
-      cb(null, uploadPath);
-    },
-    filename: (req, file, cb) => {
-      cb(null, 'model' + extname(file.originalname)); 
-    },
-  }),
-};
+//       cb(null, uploadPath);
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null, `${Date.now()}${extname(file.originalname)}`);
+//     },
+//   }),
+// };
