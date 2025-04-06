adf1ff1497faabbb44345b1c5d07baac6fa0e686
kdk1248 <alice3976@g.hongik.ac.kr>
2025-04-06 18:22:04 +0900

    refactor: export 를 통해 코드 재사용 수정 (🚧 Heritage - File Upload #45)

--- src/heritage/heritage.3dModel.moduel.ts
+++ src/heritage/heritage.3dModel.moduel.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
-import { Heritage3DModelController } from './controller/heritage.3dModel.controller';
-import { Heritage3DModelService } from './service/heritage3dModel.service';
+import { HeritageModule } from '../heritage/heritage.module'; 
 import { Heritage3DModel } from './entities/heritage3dModel.entity';
+import { Heritage } from './entities/heritage.entity';
+import { Heritage3DModelService } from './service/heritage3dModel.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Heritage3DModel]),ConfigModule ,HttpModule],
-    controllers: [Heritage3DModelController],
-    providers: [Heritage3DModelService],
+  imports: [
+    TypeOrmModule.forFeature([Heritage3DModel, Heritage]), 
+    HeritageModule,
+  ],
+  providers: [Heritage3DModelService],
+  exports: [Heritage3DModelService],
 })
 export class Heritage3DModelModule {}
