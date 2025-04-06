2caf1839cb66f38e994b7bc0ae04eb8a5b2b8958
SMK <coz0703@naver.com>
2025-04-06 17:09:52 +0900

    feat: 생성된 JWT를 Cookie에 저장

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,23 +1,35 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { AuthService } from 'src/auth/service/auth.service';
 import { CreateUserDto } from 'src/user/dto/create-user-request.dto';
 import { LoginUserDto } from 'src/user/dto/login-user.dto';
 import { UserResponseDto } from 'src/user/dto/user-response.dto';
+import { Response } from 'express';
 
 @Controller('api/auth')
 export class AuthController {
-    constructor(
-        private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
     // 회원 가입 기능
     @Post('/signup')
-    createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
-        return this.authService.createUser(createUserDto);
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> 
+    {
+        const userResponseDto = new UserResponseDto(await this.authService.createUser(createUserDto))
+        return userResponseDto;
     }
 
     // 로그인 기능
     @Post('/signin')
-    logIn(@Body() loginUserDto: LoginUserDto) {
-        return this.authService.logIn(loginUserDto);
+    async logIn(@Body() loginUserDto: LoginUserDto, @Res () res:Response): Promise<void> 
+    {
+        const accessToken = await this.authService.logIn(loginUserDto);
+
+        res.cookie('Authorization', accessToken, {
+            httpOnly: true,
+            secure: false,
+            maxAge: 360000,
+            sameSite: 'none'
+        });
+
+        res.send({message: "Login Succes"});
     }
 }
