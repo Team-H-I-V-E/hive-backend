2f59d5d8949b2a10f92353654492f2c4160c2461
Kwahk <your_email@example.com>
2025-05-22 01:50:00 +0900

    feat : CulturalAssetDto 생성 (#78)

---
+++ src/home/dto/cultural-asset.dto.ts
@@ -0,0 +1,10 @@
+export class CulturalAssetDto {
+  caId: number;
+  name: string;
+  nameEng: string;
+  description: string;
+  era: string | null;
+  designationDate: string | null;
+  eraDetail: string | null;
+  imageUrl: string;
+}
